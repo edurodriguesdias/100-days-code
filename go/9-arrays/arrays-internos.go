408fc8b18c6716ac64dd6c50f1e6baa7b4731f28
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2021-11-30 22:25:18 -0300

    arrays internos

---
+++ go/9-arrays/arrays-internos.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := make([]float32, 10, 11)
+
+	slice = append(slice, 4)
+	slice = append(slice, 434)
+
+	fmt.Println(slice)
+	fmt.Println(len(slice)) //length
+	fmt.Println(cap(slice)) //capacidade
+
+}
